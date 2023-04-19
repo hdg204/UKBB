9b6bd12e63c8fd8358cb4478a1df7bb9a22e3d69
hdg204 <36624710+hdg204@users.noreply.github.com>
2023-04-19 10:45:01 +0100

    Update baseline_data.R
    A script to read in a useful table from Exeter's DNA Nexus files

--- baseline_data.R
+++ baseline_data.R
@@ -1,1 +1,12 @@
+library(devtools)
+system('dx download file-GP7pZ9jJqYjVzqxY5B4qf1zV')
+baseline_table=read.csv('data_participant.csv')
 
+names(baseline_table)=c('eid','hba1c','glucose','recruit_age','mob','yob','sex','bmi','waist','hip','alcohol_status','alcohol_freq','pack_years','smoking_status','cigs_per_day','assess_date','height')
+baseline_table$mob=match(baseline_table$mob, month.name) 
+baseline_table=baseline_table%>%mutate(dob=as.Date(paste(yob,mob,15,sep='/')))
+
+baseline_table=mutate(baseline_table,assess_date=as.Date(assess_date))
+baseline_table=mutate(baseline_table,assess_age=as.numeric(assess_date-dob)/365.25)
+
+baseline_table=baseline_table[,c(1,7,19,18,8,9,10,2,3,14,13,15,11,12)]
