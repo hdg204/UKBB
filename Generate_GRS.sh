49cbcc90e854fef77335697bbb338b149b22b676
Harry Green <36624710+hdg204@users.noreply.github.com>
2024-07-04 19:22:22 +0100

    Update Generate_GRS.sh

--- Generate_GRS.sh
+++ Generate_GRS.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 curl https://www.chg.ox.ac.uk/~gav/resources/bgen_v1.1.4-Ubuntu16.04-x86_64.tgz > bgen.tgz
 curl https://www.chg.ox.ac.uk/~gav/resources/qctool_v2.0.8-CentOS_Linux7.6.1810-x86_64.tgz > qctool.tgz
 curl https://s3.amazonaws.com/plink1-assets/plink_linux_x86_64_20231211.zip > plink.zip
