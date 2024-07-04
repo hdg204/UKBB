238b21f0350281cf20700c552e9bd85b953e3487
Harry Green <36624710+hdg204@users.noreply.github.com>
2024-07-04 16:58:35 +0100

    Update GRS_2_test.sh

--- GRS_2_test.sh
+++ GRS_2_test.sh
@@ -18,25 +18,25 @@ make install
 export PATH=$HOME/.local/bin:$PATH
 cd $home
 
-
 dx download file-Gb53yZjJj59bVvzJZy28zK7V
-
 
 alias bgenix='bgen_v1.1.4-Ubuntu16.04-x86_64/bgenix'
 alias qctool='qctool_v2.0.8-CentOS\ Linux7.6.1810-x86_64/qctool'
 alias plink='./plink'
+
+cp bgen_v1.1.4-Ubuntu16.04-x86_64/bgenix bgenix
+cp qctool_v2.0.8-CentOS\ Linux7.6.1810-x86_64 qctool
 
 
 process_chromosome() {
     i=$1
     grep -w "^$i" Prostate_Cancer_Conti_Score | awk '{print $2}' > bp${i}
     grep -wFf bp${i} "/mnt/project/Bulk/Imputation/UKB imputation from genotype/ukb22828_c${i}_b0_v3.mfi.txt" | awk '{print $2}' > ids${i}
-    bgenix -g "/mnt/project/Bulk/Imputation/UKB imputation from genotype/ukb22828_c${i}_b0_v3.bgen" -incl-rsids ids${i} | qctool -g - -filetype bgen -og chr${i}.bed
+    ./bgenix -g "/mnt/project/Bulk/Imputation/UKB imputation from genotype/ukb22828_c${i}_b0_v3.bgen" -incl-rsids ids${i} | ./qctool -g - -filetype bgen -og chr${i}.bed
 }
 
 export -f process_chromosome
 
-# Use GNU parallel to run the function for each chromosome concurrently
 seq 1 22 | parallel -j 4 process_chromosome
 
 rm mergelist.txt
@@ -45,9 +45,9 @@ do
 	echo chr${i} >> mergelist.txt
 done
 
-plink --merge-list mergelist.txt --make-bed --out merged_dataset
+./plink --merge-list mergelist.txt --make-bed --out merged_dataset
 awk 'NR>2 {print $1, $1, 0, 0, 0, 0}' "/mnt/project/Bulk/Imputation/UKB imputation from genotype/ukb22828_c1_b0_v3.sample" > merged_dataset.fam
 
 ./plink2 --bfile merged_dataset --set-all-var-ids @_# --make-bed --out merged_dataset_2
 awk 'NR>1 {print $1"_"$2,$4,$5}' Prostate_Cancer_Conti_Score > plink_score
-plink --bfile merged_dataset_2 --score plink_score 1 2 3 --out score
+./plink --bfile merged_dataset_2 --score plink_score 1 2 3 --out score
