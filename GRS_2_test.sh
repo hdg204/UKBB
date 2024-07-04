62ebe8ff6ee9eab9662e0cf361a62b154c1445d0
Harry Green <36624710+hdg204@users.noreply.github.com>
2024-07-04 18:17:37 +0100

    Update GRS_2_test.sh

--- GRS_2_test.sh
+++ GRS_2_test.sh
@@ -30,7 +30,7 @@ cp qctool_v2.0.8-CentOS\ Linux7.6.1810-x86_64 qctool
 
 process_chromosome() {
     i=$1
-    grep -w "^$i" Prostate_Cancer_Conti_Score | awk '{print $2}' > bp${i}
+    grep -w "^$i" $1 | awk '{print $2}' > bp${i}
     grep -wFf bp${i} "/mnt/project/Bulk/Imputation/UKB imputation from genotype/ukb22828_c${i}_b0_v3.mfi.txt" | awk '{print $2}' > ids${i}
     ./bgenix -g "/mnt/project/Bulk/Imputation/UKB imputation from genotype/ukb22828_c${i}_b0_v3.bgen" -incl-rsids ids${i} | ./qctool -g - -filetype bgen -og chr${i}.bed
 }
@@ -49,5 +49,5 @@ done
 awk 'NR>2 {print $1, $1, 0, 0, 0, 0}' "/mnt/project/Bulk/Imputation/UKB imputation from genotype/ukb22828_c1_b0_v3.sample" > merged_dataset.fam
 
 ./plink2 --bfile merged_dataset --set-all-var-ids @_# --make-bed --out merged_dataset_2
-awk 'NR>1 {print $1"_"$2,$4,$5}' Prostate_Cancer_Conti_Score > plink_score
+awk 'NR>1 {print $1"_"$2,$4,$5}' $1 > plink_score
 ./plink --bfile merged_dataset_2 --score plink_score 1 2 3 --out score
