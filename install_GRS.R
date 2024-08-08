af04e7e940908970b59341e0ed5e340d5f597ce2
Harry Green <36624710+hdg204@users.noreply.github.com>
2024-08-08 15:53:06 +0100

    Create install_GRS.R
    testing

---
+++ install_GRS.R
@@ -0,0 +1,6 @@
+system('curl https://raw.githubusercontent.com/hdg204/UKBB/main/GRS_step_1.sh > GRS_step_1.sh')
+system('curl https://raw.githubusercontent.com/hdg204/UKBB/main/GRS_step_2.sh > GRS_step_2.sh')
+system('chmod +777 GRS_step_1.sh')
+system('chmod +777 GRS_step_2.sh')
+system('./GRS_step_1.sh)
+system('curl https://raw.githubusercontent.com/hdg204/Rdna-nexus/main/Example_GRS > Example_GRS')
