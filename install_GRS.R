ce469edbd4940daf551b176e35f7069a2caaae95
Harry Green <36624710+hdg204@users.noreply.github.com>
2024-08-08 22:52:20 +0100

    Update install_GRS.R

--- install_GRS.R
+++ install_GRS.R
@@ -11,4 +11,19 @@ generate_grs=function(infile){
   system(paste('./GRS_step_2.sh', infile))
   score=read.csv('score.profile',sep='')
   score=score%>%rename(eid=FID,score=SCORE)%>%select(eid,CNT1,CNT2,score)
+    
+  
+  #first apply QC steps. These commands find all the relevant information in the mfi files but only for the SNPs in the GRS
+  for (i in 1:22){
+      system(paste0("awk '$1 == ",i," {print $2}' ", infile, " | grep -w -f - ../../mnt/project/Bulk/Imputation/UKB\\ imputation\\ from\\ genotype/ukb22828_c",i,"_b0_v3.mfi.txt>infotemp"))
+      system(paste0("awk '{print $0, ",i,"}' infotemp >> info"))
+  }
+  system(paste0("awk '$1 == ",23," {print $2}' ", infile, " | grep -w -f - ../../mnt/project/Bulk/Imputation/UKB\\ imputation\\ from\\ genotype/ukb22828_cX_b0_v3.mfi.txt>infotemp"))
+  system(paste0("awk '{print $0, ",23,"}' infotemp >> info"))
+  system(paste0("awk '$1 == ",23," {print $2}' ", infile, " | grep -w -f - ../../mnt/project/Bulk/Imputation/UKB\\ imputation\\ from\\ genotype/ukb22828_cX_b0_v3.mfi.txt>infotemp"))
+  system(paste0("awk '{print $0, ",23,"}' infotemp >> info"))
+  info=read.csv('info',sep='',header=F)
+  info=info%>%rename(ID=V1,rsid=V2,MAF=V6,info=V8)%>%select(ID,rsid,MAF,info)
+  return(c(score,info))
 }
+
