73e10aa5a0b87818eafd0ed5e606eab7555a1017
Harry Green <36624710+hdg204@users.noreply.github.com>
2024-08-08 19:01:25 +0100

    Update install_GRS.R

--- install_GRS.R
+++ install_GRS.R
@@ -5,7 +5,10 @@ system('chmod +777 GRS_step_2.sh')
 system('./GRS_step_1.sh')
 system('curl https://raw.githubusercontent.com/hdg204/Rdna-nexus/main/Example_GRS > Example_GRS')
 
-
+library('dplyr')
+library('devtools')
 generate_grs=function(infile){
   system(paste('./GRS_step_2.sh', infile))
+  score=read.csv('score.profile',sep='')
+  score=score%>%rename(eid=FID,score=SCORE)%>%select(eid,CNT1,CNT2,score)
 }
