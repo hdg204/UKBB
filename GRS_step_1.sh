9100ab8f56eeeacdc4a7b4ec175cc51211bf0f56
Harry Green <36624710+hdg204@users.noreply.github.com>
2024-08-08 15:50:37 +0100

    Create GRS_step_1.sh
    TESTING ONLY

---
+++ GRS_step_1.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+curl https://www.chg.ox.ac.uk/~gav/resources/bgen_v1.1.4-Ubuntu16.04-x86_64.tgz > bgen.tgz
+curl https://www.chg.ox.ac.uk/~gav/resources/qctool_v2.0.8-CentOS_Linux7.6.1810-x86_64.tgz > qctool.tgz
+curl https://s3.amazonaws.com/plink1-assets/plink_linux_x86_64_20231211.zip > plink.zip
+
+
+curl -s https://s3.amazonaws.com/plink2-assets/ > plinkweb
+grep -oP 'plink2_linux_x86_64_\d{8}.zip' plinkweb | sort | tail -n 1 > plinkfile
+curl -o plink2.zip $(paste -d "" <(echo -n "https://s3.amazonaws.com/plink2-assets/") plinkfile)
+
+curl https://s3.amazonaws.com/plink2-assets/plink2_linux_x86_64_20240806.zip > herpderp
+
+tar -xvzf bgen.tgz
+tar -xvzf qctool.tgz
+unzip -o plink.zip
+unzip -o plink2.zip
+
+curl https://ftp.gnu.org/gnu/parallel/parallel-latest.tar.bz2 > parallel-latest.tar.bz2
+tar -xjf parallel-latest.tar.bz2
+
+target_directory=$(find . -type d -name 'parallel-202*' -print -quit)
+cd "$target_directory"
+
+
+pwd
+./configure --prefix=$HOME/.local
+make
+make install
+export PATH=$HOME/.local/bin:$PATH
+cd $home
+
+dx download file-Gb53yZjJj59bVvzJZy28zK7V
+
+alias bgenix='bgen_v1.1.4-Ubuntu16.04-x86_64/bgenix'
+alias qctool='qctool_v2.0.8-CentOS\ Linux7.6.1810-x86_64/qctool'
+alias plink='./plink'
+
+cp bgen_v1.1.4-Ubuntu16.04-x86_64/bgenix bgenix
+cp qctool_v2.0.8-CentOS\ Linux7.6.1810-x86_64/qctool qctool
